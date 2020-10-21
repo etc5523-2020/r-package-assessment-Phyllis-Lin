78ce94fbf2443ef06bc431dc1b598a58548c49ca
Peimin <plin0018@student.monash.edu>
2020-10-22 01:21:47 +1100

    add shiny function

--- R/launch.R
+++ R/launch.R
@@ -1,6 +1,9 @@
+#' Launch Phyllis first shinyapp
+#' 
+#' 
 #' @export
 launch_app <- function() {
-  appDir <- system.file("app", "myapp", package = "phyllispkg")
+  appDir <- system.file("app", package = "phyllispkg")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `phyllispkg`.", call. = FALSE)
   }
