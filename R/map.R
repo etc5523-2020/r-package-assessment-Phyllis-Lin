cbc688a7245aafc36b939c8cbbc8aa1e0970049e
Peimin <plin0018@student.monash.edu>
2020-10-21 23:30:43 +1100

    add dsicription of map data

---
+++ R/map.R
@@ -0,0 +1,7 @@
+#' Covid-19 map 
+#'
+#' This data set contains longitude and latitude of 10 main countries 
+#'
+#' @source
+#' The data are provided by the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE) and Rami Krispin and other 4 contributor provided a tidy format of the data as r package coronavirus.
+"map"
